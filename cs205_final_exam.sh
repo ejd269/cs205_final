3640152a589c67be5bd788f2710f93e1560c75f6
Eric <ejd269@nau.edu>
2024-05-07 19:03:08 -0700

    updated cs_205_final_exam.sh and created screenshot folder with screenshot of running command inside it

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like best_pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
 #    File name: [VALUE]
@@ -10,3 +12,24 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+
+#establish variables
+file="$1"
+total_pokemon=
+avg_hp=
+avg_attack=
+
+#print summary title 
+printf "===== SUMMARY OF DATA FILE ====="
+#print file name
+printf "\n   File name: %s" $file
+#print total pokemon
+printf "\n   Total Pokemon: %d" $total_pokemon
+#print average hp
+printf "\n   Avg. HP: %d" $avg_hp
+#print average attack
+printf "\n   Avg. Attack: %d" $avg_attack
+#print end summary message
+printf "\n===== END SUMMARY =====\n"
+
+
